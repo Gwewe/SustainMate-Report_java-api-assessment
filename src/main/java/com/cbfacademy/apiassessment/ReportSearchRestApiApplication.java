0a347b296af27bef68140308e28bb5b04b915077
Wedna Guirand <wedna.guirand@gmail.com>
2024-06-05 09:19:20 +0100

    setting up the application properties files and edit the ReportSearchRestApiApplication file.

---
+++ src/main/java/com/cbfacademy/apiassessment/ReportSearchRestApiApplication.java
@@ -0,0 +1,15 @@
+package com.cbfacademy.apiassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportSearchRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportSearchRestApiApplication.class, args);
+	}
+
+	
+
+}
