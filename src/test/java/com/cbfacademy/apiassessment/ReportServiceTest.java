786f7ce7d66388bfe03ba686b326f71696fc136e
Wedna Guirand <wedna.guirand@gmail.com>
2024-07-02 08:14:25 +0100

    remove unused import

--- src/test/java/com/cbfacademy/apiassessment/ReportServiceTest.java
+++ src/test/java/com/cbfacademy/apiassessment/ReportServiceTest.java
@@ -15,8 +15,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-// import static org.mockito.Mockito.times;
-// import static org.mockito.Mockito.verify;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import com.cbfacademy.apiassessment.reports.Category;
